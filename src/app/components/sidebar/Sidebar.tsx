8a73e9e6f27047724e9e38f2a29c843e55f5879f
Bikra123 <bikraabn@gmail.com>
2025-07-23 09:14:17 +0700

    first commit

---
+++ src/app/components/sidebar/Sidebar.tsx
@@ -0,0 +1,12 @@
+import { Drawer } from 'antd'
+import React from 'react'
+
+const Sidebar = () => {
+  return (
+    <div>
+      <Drawer title="Basic Drawer"/>
+    </div>
+  )
+}
+
+export default Sidebar
